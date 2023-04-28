f9e22ebbe3b002814beb602f9d4b3009df2b77ea
Bruno Breggia <breggiabruno@gmail.com>
2023-04-28 17:43:28 -0300

    Particion del dataset ahora se hace dentro del metodo fit. La generacion de batches tiene opcion de shuffle.

--- mine/mine2.py
+++ mine/mine2.py
@@ -12,24 +12,33 @@ os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 # TODO: add progress bar with tqdm
 
 
-def generate_minibatches(trainig_set: torch.tensor, size: int):
+def generate_minibatches(trainig_set: torch.tensor, size: int, shuffle=False):
     """
     Function that returns minibatches from an original trainign dataset,
     of solicited size.
 
     Parameters
     ----------
-    trainig_set: torch.tensor
+    trainig_set : torch.tensor
         The complete dataset used for training, to be splitted into minibatches.
-    size: int
+    size : int
         The size of the individual minibatches. If length of dataset is not
         a multiple of the size, then last minibatch may contain less data.
+    shuffle : bool
+        Flag that indicates if dataset should be suffled before splitting
+        in minibatches
 
     Return
     ------
-    minibatches: torch.tensor
+    minibatches : torch.tensor
         A tensor of minibatches of demanded size.
     """
+
+    if shuffle:
+        size = trainig_set.shape[0]
+        rand_perm = torch.randperm(size)
+        trainig_set = trainig_set[rand_perm]
+
     minibatches = trainig_set.split(size, dim=0)
     return minibatches
 
@@ -52,13 +61,13 @@ class Mine2(nn.Module):
         """
         Parameters
         ----------
-        hidden_layers: int
+        hidden_layers : int
             Amount of hidden layers for the network. The input layer will
             be of 2 dimensions (for the 2 input signals), and the output will
             be of 1 dimension. Minimum value permitted: 1.
-        neurons: int
+        neurons : int
             Amount of neurons per hidden layer.
-        cuda: str
+        cuda : str
             Specify where the model should run, on cpu or in graphic processor.
             If None, it will run on gpu if found, else on cpu.
         """
@@ -103,12 +112,12 @@ class Mine2(nn.Module):
 
         Parameters
         ----------
-        input: torch.tensor
+        input : torch.tensor
             Batch of input data to pass into the model
 
         Returns
         -------
-        output: torch.tensor
+        output : torch.tensor
             Tensor with the output of the model
         """
 
@@ -127,13 +136,13 @@ class Mine2(nn.Module):
 
         Parameters
         ----------
-        train_dataset: torch.tensor
+        train_dataset : torch.tensor
             Input to the model. It consists of the concatenation of the
             two signals to analyze.
 
         Returns
         -------
-        loss: torch.tensor
+        loss : torch.tensor
             Tensor of one dimension which contains the loss of the network.
             The loss is actually the negative of the value we really want
             to maximize, which is the mutual information estimation.
@@ -167,12 +176,12 @@ class Mine2(nn.Module):
 
         Parameters
         ----------
-        input_dataset: torch.tensor
+        input_dataset : torch.tensor
             Input data to the model.
 
         Return
         ------
-        mi_estimation: torch.tensor
+        mi_estimation : torch.tensor
             Output of the model, i.e the mutual information estimation.
         """
 
@@ -198,7 +207,12 @@ class Mine2(nn.Module):
 
         return mi_estimation
 
-    def fit(self, train_loader, val_loader, num_epochs, learning_rate=1e-4, show_progress=True):
+    def fit(self, signal_x: torch.tensor, signal_z: torch.tensor,
+            num_epochs: int,
+            train_percent: int = 80,
+            minibatch_size: int = 1,
+            learning_rate: float = 1e-4,
+            show_progress: bool = False):
         """
         Trains the MINE model using the provided training data loader and
         validates it using the provided validation data loader.
@@ -209,60 +223,91 @@ class Mine2(nn.Module):
 
         Parameters
         ----------
-        train_loader: torch.tensor
-            A PyTorch DataLoader object representing the training data.
-        val_loader: torch.tensor
-            A PyTorch DataLoader object representing the validation data.
-        num_epochs: int
+        signal_x : torch.tensor
+            First signal to use to find mutual information.
+        signal_z : torch.tensor
+            Second signal to use to find mutual information.
+        num_epochs : int
             An integer specifying the number of epochs to train the model for.
-        learning_rate: float
+        train_percent : int
+            Percentage of dataset to use for training. Remaining will be used
+            for validation. Default value is 80.
+        minibatch_size : int
+            Size of minibatches to be created during training.
+        learning_rate : float
             A float specifying the learning rate to use for the optimizer.
-        show_progress: bool
+        show_progress : bool
             Flag to turn on the percetage of training progress-bar.
         """
         optimizer = torch.optim.SGD(self.model.parameters(), lr=learning_rate)
 
+        input_dataset = torch.cat((signal_x, signal_z), dim=1)
+        train_size = int(len(input_dataset) * train_percent / 100)
+        val_size = len(input_dataset) - train_size
+
+        # TODO: obtain the validation portion of signal from randomly selected index.
+        #  The remaining portions of data should be joined and used for training.
+        train_dataset, val_dataset = input_dataset.split([train_size, val_size], dim=0)
+
         for epoch in tqdm(range(num_epochs), disable=not show_progress):
 
             # Training loop #
-            for batch in train_loader:
+            for batch in generate_minibatches(train_dataset, size=minibatch_size, shuffle=True):
                 loss = self.training_step(batch)
                 loss.backward()
                 optimizer.step()
                 optimizer.zero_grad()
 
             # Validation #
-            # with torch.no_grad():
             # TODO Process the signal in validation_progress and cut the
             #  training epochs when this signal is decreasing
-            # Training queue
-            result_train = self.evaluate(torch.cat(train_loader, dim=0))
-            if len(self.training_progress) >= self.k:
-                self.training_progress.pop(0)
+            # Raw training signal
+            result_train = self.evaluate(train_dataset)
             self.training_progress.append(result_train.item())
-            # Training smoothing
+            # Smoothed training signal
             if len(self.training_filtered) == 0:
                 self.training_filtered.append(result_train)
             else:
+                ref_value = self.training_progress[(-self.k) if len(self.training_progress) > self.k else 0]
                 self.training_filtered.append(
-                    self.training_filtered[-1] + (result_train - self.training_progress[0]) / self.k
+                    self.training_filtered[-1] + (result_train - ref_value) / self.k
                 )
-            # Validation queue
-            result_val = self.evaluate(val_loader)
-            if len(self.validation_progress) >= self.k:
-                self.validation_progress.pop(0)
+            # Raw validation signal
+            result_val = self.evaluate(val_dataset)
             self.validation_progress.append(result_val.item())
-            # Validation smoothing
+            # Smoothed validation signal
             if len(self.validation_filtered) == 0:
                 self.validation_filtered.append(result_val)
             else:
+                ref_value = self.validation_progress[(-self.k) if len(self.validation_progress) > self.k else 0]
                 self.validation_filtered.append(
-                    self.validation_filtered[-1] + (result_val-self.validation_progress[0])/self.k
+                    self.validation_filtered[-1] + (result_val-ref_value)/self.k
                 )
 
-    def plot_training(self, true_mi: int = None, save=False):
-        plt.plot(self.training_filtered, color='b', label='Training')
-        plt.plot(self.validation_filtered, color='g', label='Validation')
+    def plot_training(self, true_mi: float = None, smooth: bool = True, save: bool = False):
+        """
+        Plots the progress of training and of validation process during the training epochs.
+        The curves are actually smoothed out with moving average filter.
+
+        Parameters
+        ----------
+        true_mi : float
+            True value of mutual information or value of reference we want to
+            show in the plot.
+        smooth : bool
+            True if you want to plot the smoothed versions of the training and
+            validation signals.
+        save : bool
+            Flag that indicates if we want to save the plot to a pdf file.
+        """
+
+        if smooth:
+            plt.plot(self.training_filtered, color='b', label='Training')
+            plt.plot(self.validation_filtered, color='g', label='Validation')
+        else:
+            plt.plot(self.training_progress, color='b', label='Training')
+            plt.plot(self.validation_progress, color='g', label='Validation')
+
         if true_mi is not None:
             plt.axhline(y=true_mi, color='r', linestyle='-')
         plt.ylabel('Mutual information estimation')
@@ -289,21 +334,14 @@ if __name__ == "__main__":
     # Convert to tensors
     x = torch.from_numpy(X_samples).float().to(device='cpu')
     z = torch.from_numpy(Z_samples).float().to(device='cpu')
-    # Entropia mediante formula
+    # Informacion mutua mediante formula
     true_mi = -0.5 * np.log(np.linalg.det(cov_matrix))
     print(f"The real mutual information is {true_mi}")
 
-    MINE = Mine2(1, 100)
-
-    input_dataset = torch.cat((x, z), dim=1)
-    TRAIN_PERCENT = 80  # 80 percent of input dataset is saved for training, remaining for validation
-    train_size = int(len(input_dataset)*TRAIN_PERCENT/100)
-    val_size = len(input_dataset) - train_size
-    train_dataset, val_dataset = input_dataset.split([train_size, val_size], dim=0)
-    training_minibatches = generate_minibatches(train_dataset, size=100)
+    MINE = Mine2(3, 100)
 
     tic = time.time()
-    MINE.fit(training_minibatches, val_dataset, 10000)
+    MINE.fit(x, z, num_epochs=10000, minibatch_size=500, learning_rate=5e-4, show_progress=True)
     toc = time.time()
 
     MINE.plot_training(true_mi)
